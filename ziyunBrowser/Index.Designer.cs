32d5b5e0300bdc3bd0ae72e31b31692cb374a14e
Syears <532113394@qq.com>
2019-01-16 15:24:02 +0800

    子云浏览器第一次提交

---
+++ ziyunBrowser/Index.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ziyunBrowser
+{
+    partial class Index
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Index));
+            this.ziyunBrowerContent = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ziyunBrowerContent
+            // 
+            this.ziyunBrowerContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ziyunBrowerContent.Location = new System.Drawing.Point(0, 79);
+            this.ziyunBrowerContent.Name = "ziyunBrowerContent";
+            this.ziyunBrowerContent.Size = new System.Drawing.Size(1109, 525);
+            this.ziyunBrowerContent.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "测试";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1109, 604);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ziyunBrowerContent);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Name = "Index";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "子云";
+            this.Load += new System.EventHandler(this.Index_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ziyunBrowerContent;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ziyunBrowser/Index.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ziyunBrowser
+{
+    public partial class Index : Form
+    {
+        public WebKit.WebKitBrowser browser = null;
+        public Index()
+        {
+            InitializeComponent();
+        }
+
+        private void Index_Load(object sender, EventArgs e)
+        {
+            browser = new WebKit.WebKitBrowser();
+            browser.Dock = DockStyle.Fill;
+            browser.AllowNavigation = true;
+            browser.AllowNewWindows = true;
+            
+            this.ziyunBrowerContent.Controls.Add(browser);
+            browser.Navigate("https://www.baidu.com");
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            browser.AllowNavigation = true;
+        }
+    }
+}
